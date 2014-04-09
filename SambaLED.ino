d2e92d8b68e8a0fb04d5f537aced6b1320297f70
dwatremez <donatien.watremez@gmail.com>
2014-04-09 17:30:00 +0200

    Traduction en anglais

--- SambaLED.ino
+++ SambaLED.ino
@@ -20,142 +20,142 @@ enum {
 #define INSTRU AGOGO
 
 // Forme de l'instrument
-#define TYPE_INSTRUMENT_ROND 0
-#define TYPE_INSTRUMENT_DROIT 1
+#define CIRCULAR 0
+#define BAR 1
 
 // Nombre de LED par defaut
-#define NOMBRE_LEDS_AGOGO 5
-#define NOMBRE_LEDS_CHOCA 7
-#define NOMBRE_LEDS_CAIXA 28
-#define NOMBRE_LEDS_CUICA 15 // à définir
-#define NOMBRE_LEDS_REPIQUE 28
-#define NOMBRE_LEDS_SURDO 42
-#define NOMBRE_LEDS_TAMBORIM 13
+#define NB_LED_AGOGO 5
+#define NB_LED_CHOCA 7
+#define NB_LED_CAIXA 28
+#define NB_LED_CUICA 15 // à définir
+#define NB_LED_REPIQUE 28
+#define NB_LED_SURDO 42
+#define NB_LED_TAMBORIM 13
 
 // Nombre de LED exceptionnel
-#define NOMBRE_LEDS_SURDO_PETIT 40
-#define NOMBRE_LEDS_TAMBORIM_PETIT 12
+#define NB_LED_SURDO_PETIT 40
+#define NB_LED_TAMBORIM_PETIT 12
 
 // Luminosité
-#define LUM_SURDO 255
-#define LUM_CAIXA 255
-#define LUM_REPIQUE 255
-#define LUM_CUICA 255
-#define LUM_AGOGO 150 // à définir
-#define LUM_CHOCA 200 // à définir
-#define LUM_TAMBORIM 150 // à définir
+#define BRIGHTNESS_SURDO 255
+#define BRIGHTNESS_CAIXA 255
+#define BRIGHTNESS_REPIQUE 255
+#define BRIGHTNESS_CUICA 255
+#define BRIGHTNESS_AGOGO 150 // à définir
+#define BRIGHTNESS_CHOCA 200 // à définir
+#define BRIGHTNESS_TAMBORIM 150 // à définir
 
 // Messages de la télécommande
-#define TOUCHE_1 5391
-#define TOUCHE_2 5392
-#define TOUCHE_3 5393
-#define TOUCHE_4 5394
-#define TOUCHE_5 5395
-#define TOUCHE_6 5396
-#define TOUCHE_7 5397
-#define TOUCHE_8 5398
-#define TOUCHE_9 5399
-#define TOUCHE_0 5390
-#define TOUCHE_ETOILE 5910
-#define TOUCHE_DIESE 5920
+#define KEY_1 5391
+#define KEY_2 5392
+#define KEY_3 5393
+#define KEY_4 5394
+#define KEY_5 5395
+#define KEY_6 5396
+#define KEY_7 5397
+#define KEY_8 5398
+#define KEY_9 5399
+#define KEY_0 5390
+#define KEY_ETOILE 5910
+#define KEY_DIESE 5920
 
 // Definition de l'instrument
-struct caracInstrument{
-  char nom;
+struct defineInstrument{
+  char name;
   int type;
   int nbLED;
-  int lum;
+  int brightness;
 };
 
 
 void setup()
 {
-  struct caracInstrument instrument;
+  struct defineInstrument instrument;
 
-  instrument.nom = INSTRU;
+  instrument.name = INSTRU;
   switch (INSTRU) {
   case AGOGO:
-    instrument.type = TYPE_INSTRUMENT_DROIT;
-    instrument.nbLED = NOMBRE_LEDS_AGOGO;
-    instrument.lum = LUM_AGOGO;
+    instrument.type = BAR;
+    instrument.nbLED = NB_LED_AGOGO;
+    instrument.brightness = BRIGHTNESS_AGOGO;
     break;
   case SURDO1:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_SURDO;
-    instrument.lum = LUM_SURDO;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_SURDO;
+    instrument.brightness = BRIGHTNESS_SURDO;
     break;
   case SURDO2:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_SURDO;
-    instrument.lum = LUM_SURDO;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_SURDO;
+    instrument.brightness = BRIGHTNESS_SURDO;
     break;
   case SURDO3:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_SURDO;
-    instrument.lum = LUM_SURDO;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_SURDO;
+    instrument.brightness = BRIGHTNESS_SURDO;
     break;
   case CAIXA:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_CAIXA;
-    instrument.lum = LUM_CAIXA;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_CAIXA;
+    instrument.brightness = BRIGHTNESS_CAIXA;
     break;
   case REPIQUE:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_REPIQUE;
-    instrument.lum = LUM_REPIQUE;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_REPIQUE;
+    instrument.brightness = BRIGHTNESS_REPIQUE;
     break;
   case CHOCALHO:
-    instrument.type = TYPE_INSTRUMENT_DROIT;
-    instrument.nbLED = NOMBRE_LEDS_CHOCA;
-    instrument.lum = LUM_CHOCA;
+    instrument.type = BAR;
+    instrument.nbLED = NB_LED_CHOCA;
+    instrument.brightness = BRIGHTNESS_CHOCA;
     break;
   case CUICA:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_CUICA;
-    instrument.lum = LUM_CUICA;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_CUICA;
+    instrument.brightness = BRIGHTNESS_CUICA;
     break;
   case TAMBORIM:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_TAMBORIM;
-    instrument.lum = LUM_TAMBORIM;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_TAMBORIM;
+    instrument.brightness = BRIGHTNESS_TAMBORIM;
     break;
   case SURDO2_PETIT:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_SURDO_PETIT;
-    instrument.lum = LUM_SURDO;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_SURDO_PETIT;
+    instrument.brightness = BRIGHTNESS_SURDO;
     break;
   case TAMBORIM_PETIT:
-    instrument.type = TYPE_INSTRUMENT_ROND;
-    instrument.nbLED = NOMBRE_LEDS_TAMBORIM_PETIT;
-    instrument.lum = LUM_TAMBORIM;
+    instrument.type = CIRCULAR;
+    instrument.nbLED = NB_LED_TAMBORIM_PETIT;
+    instrument.brightness = BRIGHTNESS_TAMBORIM;
     break;
   default:
-    instrument.type = TYPE_INSTRUMENT_DROIT;
+    instrument.type = BAR;
     instrument.nbLED = 1;
-    instrument.lum = 100;
+    instrument.brightness = 100;
     break;
   }
 
 
   // Définition du bandeau de LED
-  Adafruit_NeoPixel bande = Adafruit_NeoPixel(instrument.nbLED, LEDPIN, NEO_GRB + NEO_KHZ800);
+  Adafruit_NeoPixel strip = Adafruit_NeoPixel(instrument.nbLED, LEDPIN, NEO_GRB + NEO_KHZ800);
 
   // Définition des couleurs utilisées
   uint32_t sambaColor[3] = {
-    bande.Color(120, 130, 18),
-    bande.Color(255, 44, 15),
-    bande.Color(30, 120, 200)
+    strip.Color(120, 130, 18),
+    strip.Color(255, 44, 15),
+    strip.Color(30, 120, 200)
   };
 
-  uint32_t off = bande.Color(0,0,0);
+  uint32_t off = strip.Color(0,0,0);
 
 
   //Serial.begin(9600);
   //Serial.println("Script Led");
 
   // Initialiser NeoPixel
-  bande.begin();
-  bande.setBrightness(instrument.lum);
+  strip.begin();
+  strip.setBrightness(instrument.brightness);
 
   // Initialiser Communication RF
   mySwitch.enableReceive(0);
