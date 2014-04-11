5c863943d3e71dfd7f0946ff82302f7760b4b8dc
dwatremez <donatien.watremez@gmail.com>
2014-04-11 10:40:54 +0200

    Mise en place d'animations complètes (Mangueira, Brasil, Girls, Geo, Drums, Wave, Sambagogo)

--- SambaLED/SambaLED.ino
+++ SambaLED/SambaLED.ino
@@ -546,3 +546,149 @@ void loop()
 	}
 
 }
+
+
+// Animations Complètes
+
+void Brasil()  // 6
+{
+      if(instrument.type == BAR)
+      {
+            if(animDirection == 0)
+            {
+                  colorWipe(sambaYellow);
+                  if(indexLED == instrument.nbLED - 1)
+                        animDirection = 1;
+            }
+            else
+            {
+                  colorWipe(sambaGreen);
+                  if(indexLED == instrument.nbLED - 1)
+                        animDirection = 0;
+            }     
+      }
+      
+      if(instrument.type == CIRCULAR || instrument.name == SURDO3)
+      {
+            colorPathForward(sambaYellow, off, instrument.lenght);
+            colorPathBackward(sambaGreen, off, instrument.lenght);           
+
+      }
+      
+      if(instrument.name == SURDO1)
+            colorPathForward(sambaYellow, off, instrument.lenght);
+            
+      if(instrument.name == SURDO2)
+            colorPathBackward(sambaGreen, off, instrument.lenght);
+}
+
+void Mangueira()  // 1
+{
+      if(instrument.type == BAR)
+            colorPathBackForth(sambaFushia, sambaGreen, 1);
+      
+      if(instrument.type == CIRCULAR || instrument.name == SURDO3)
+            colorPathForward(sambaGreen, sambaFushia, 0.5 * instrument.nbLED);
+      
+      if(instrument.name == SURDO1)
+            colorPathForward(sambaFushia, off, 0.5 * instrument.nbLED);
+            
+      if(instrument.name == SURDO2)
+            colorPathForward(sambaGreen, off, 0.5 * instrument.nbLED);
+      
+}
+
+void Girls()
+{
+  
+      if(instrument.type == BAR)
+            colorGauge(sambaEmeraud, sambaHotPink);
+      if(instrument.type == CIRCULAR)
+            colorGauge(sambaEmeraud, sambaHotPink);
+            
+      if(instrument.name == SURDO1)
+            colorPathBackward(sambaEmeraud, off, instrument.lenght);
+      if(instrument.name == SURDO2)
+            colorPathForward(sambaHotPink, off, instrument.lenght);  
+      
+      if(instrument.name == SURDO3)
+      {
+            if(animDirection == 0)
+            {
+                  colorPathForward(sambaEmeraud, off, instrument.lenght);
+                  if(indexLED == instrument.nbLED - 1)
+                        animDirection = 1;
+            }
+            else
+            {
+                  colorPathBackward(sambaHotPink, off, instrument.lenght);
+                  if(indexLED == instrument.nbLED - 1)
+                        animDirection = 0;
+            }     
+      }
+            
+}
+
+
+void Geo()
+{
+    if(instrument.place == FIRST)
+          colorPathBackForth(sambaYellow, off, instrument.lenght);
+
+    if(instrument.place == BETWEEN)
+  	  colorPathForward(sambaBlue, off, instrument.lenght);
+			
+    if(instrument.place == LAST)
+    	  colorPathForward(sambaRed, off, instrument.lenght);
+			
+}
+
+void Sambagogo()
+{
+    if(instrument.name == AGOGO)
+          colorWipe(sambaRed);
+    
+    if(instrument.name == TAMBORIM || instrument.name == TAMBORIM_SMALL)
+          colorWipe(sambaYellow);
+    
+    if(instrument.name == CHOCALHO)
+          colorWipe(sambaCarrot);
+    
+    if(instrument.name == CAIXA)
+          colorWipe(sambaCarrot);
+    
+    if(instrument.name == REPIQUE)
+          colorWipe(sambaYellow);
+    
+    if(instrument.type == BIG_CIRCULAR)
+          colorWipe(sambaRed);
+}
+
+void Drums()
+{
+      if(instrument.name != CAIXA && instrument.name != REPIQUE)
+      {
+           colorWipe(off);
+      }
+      else
+      {          
+            colorPathForward(sambaCarrot, off, instrument.lenght);
+            colorPathBackward(sambaRed, off, instrument.lenght);          
+      }
+  
+      
+}
+
+void Wave()
+{
+      if(instrument.type != BIG_CIRCULAR)
+      {
+            colorWipe(off);
+      }
+      else
+      {
+            colorPathForward(sambaEmeraud, off, instrument.lenght);
+            colorPathBackward(sambaBlue, off, instrument.lenght);
+            
+      }
+}
